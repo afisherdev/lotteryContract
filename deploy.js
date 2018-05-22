0a563ce54098f5a15db92006d8dfc718b799483a
Andrew <andrewdavidfisher@gmail.com>
2018-05-22 10:55:04 -0700

    fixed tests and added to deploy.js

--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ const deploy = async () => {
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: '0x' + bytecode })
     .send({ gas: '1000000', gasPrice: web3.utils.toWei('2', 'gwei'), from: accounts[0] })
-
+  console.log(interface)
   console.log('Contract deployed to: ', result.options.address);
 };
 deploy();
